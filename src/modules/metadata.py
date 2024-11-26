8b9dbd3d02d698182e453b6c54087574483c7179
Wiktor <stwiktor65@gmail.com>
2024-11-26 06:45:54 -0800

    check_path_type to check if if given path is valid as a file or dir

--- src/modules/metadata.py
+++ src/modules/metadata.py
@@ -1,3 +1,4 @@
+from utils.helper import check_path_type, resolve_path
 import click
 
 @click.group()
@@ -11,11 +12,7 @@ def module():
 @click.option("--date-less-than", type=click.DateTime(), help="Find files with dates earlier than this.")
 @click.option("--date-greater-than", type=click.DateTime(), help="Find files with dates later than this.")
 @click.pass_context
-def search_by_date(ctx, date_less_than, date_greater_than):
+def search_by_date(ctx:click.Context, date_less_than, date_greater_than, path):
     """
     Search files by metadata dates.
     """
-    workdir = ctx.obj.get("workdir")
-    click.echo(f"Working directory: {workdir}")
-    click.echo(f"Searching for files with date criteria...")
-    # Add logic to process files using date filters
--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import click
 
 def run_command(command):
     """Run a shell command and return the output."""
@@ -8,7 +9,7 @@ def run_command(command):
         raise Exception(f"Command failed: {result.stderr}")
     return result.stdout.strip()
 
-def resolve_path(path, ctx_path):
+def resolve_path(ctx_path, path):
     """
     Resolve a given path relative to the current working directory.
 
@@ -22,3 +23,22 @@ def resolve_path(path, ctx_path):
     else:
         # Otherwise, resolve it relative to the current working directory
         return os.path.abspath(os.path.join(ctx_path, path))
+
+def check_path_type(ctx_path, path, has_to_be_file):
+    """
+        Determine if the given path is a file or a directory after resolving it.
+        :param ctx_path: Context of the current working directory .
+        :param path: Path to check.
+        :param has_to_be_file: Boolean to determine if the path is a file or a directory.
+        :raises BadParameter: If the path of certain type is invalid.
+    """
+    resolved_path = resolve_path(ctx_path, path)
+
+    if not os.path.exists(resolved_path):
+        raise click.BadParameter(f"Invalid path: {resolved_path}")
+
+    if not has_to_be_file and os.path.isfile(resolved_path):
+        raise click.BadParameter(f"Given path is not a directory: {resolved_path}")
+
+    elif has_to_be_file and os.path.isdir(resolved_path):
+        raise click.BadParameter(f"Given path is not a file: {resolved_path}")
